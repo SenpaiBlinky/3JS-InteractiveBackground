93f421e86522b42f62bc8d6c9f7139ec3e8da339
Master <blinkerion@gmail.com>
2021-06-19 09:09:34 -0500

    da

--- MyIntBackground/main.js
+++ MyIntBackground/main.js
@@ -421,7 +421,7 @@ button2.addEventListener("click", (event) => {
     workContainer.style.visibility = "hidden"
 
     // purpleBackground()
-    // animate() -daa
+    // animate() 
     
     camera.position.z = -12
     planeMesh.position.z = -16
